91b9c6b2662229d659e3d7e5b9f5597348f1411d
ygtankara <ankaraygt@gmail.com>
2025-08-16 03:50:09 +0300

    phase2

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
+
+let btn = document.querySelector("button");
+let first = document.getElementById("first");
+let second= document.getElementById("second");
+
+let passLength = 16
+
+btn.addEventListener("click", function(){
+
+    first.textContent = " "
+    second.textContent = " "
+
+    for (i=0; i<passLength; i++) {
+
+        
+        let x = Math.floor(Math.random()*characters.length)
+        first.textContent += characters[x] /*first.textContent = first.textContent + characters[x]*/
+        second.textContent += characters[x]
+        
+    }
+});
+
+
